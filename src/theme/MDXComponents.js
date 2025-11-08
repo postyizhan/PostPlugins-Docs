8bd8a65db0f280d67b262cebc7dff550a651acc8
si <61087334+Lythrilla@users.noreply.github.com>
2025-11-08 23:47:08 +0800

    Delete src directory

--- src/theme/MDXComponents.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MDXComponents from '@theme-original/MDXComponents';
-import ResourcePageMDX from '@site/src/components/ResourceComponents/ResourcePageMDX';
- 
-export default {
-  ...MDXComponents,
-  ResourcePage: ResourcePageMDX,
-}; 
